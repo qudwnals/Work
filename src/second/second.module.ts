2f0e204d655feaac0e2bf9bcb8bc685697006392
qudwnals <163666385+qudwnals@users.noreply.github.com>
2024-10-06 15:17:54 +0900

    Delete src/second directory

--- src/second/second.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SecondService } from './second.service';
-import { SecondController } from './second.controller';
-
-@Module({
-  providers: [SecondService],
-  controllers: [SecondController]
-})
-export class SecondModule {}
